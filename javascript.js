f553a82f6ca312cfe402d4aa72c0ec7a0bb26e26
janbornindec <janellecwx@gmail.com>
2023-04-08 12:01:09 +1000

    Display result in new div

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
-//make three choices into an object's own enumerable string-keyed property key-value pairs
-const choices = {0: 'rock', 1: 'paper', 2: 'scissors'};
+//make three choices
+const choices = ['rock','paper','scissors'];
 
 //computer to choose randomly and return as computerSelection
 function getComputerChoice() {
-    computerSelection = Math.floor(Math.random() * 3);
+    computerSelection = choices[Math.floor(Math.random() * 3)];
     return computerSelection;
 };
 
@@ -12,9 +12,16 @@ const buttons = document.querySelectorAll('.btn');
 buttons.forEach((button) => button.style.display = 'none');
 
 //make game options visible once StartGame btn is clicked
-document.querySelector('.start').addEventListener('click', showBtn); 
+document.querySelector('.start').addEventListener('click', newGame); 
 
-function showBtn(e) {
+let gameOn = false; //game is not started if startGame is not clicked
+//default stats
+let round = 0;
+let userWon = 0;
+let computerWon = 0;
+
+function newGame(e) {
+    gameOn = true; //start new game
     buttons.forEach((button) => button.style.display = 'block');
     e.preventDefault(); //
     document.querySelector('.start').style.display='none'; //make startgame btn disappear
@@ -24,19 +31,11 @@ function showBtn(e) {
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         userSelection = button.id;
-        return userSelection;
+        playRound(userSelection);
     });
 });
 
-//user to input their choice
-function playRound() {
-    getComputerChoice();
-    chooseWinner();
-};
-
 //compare both choices, announce the winner
-let userWon = 0;
-let computerWon = 0;
 function chooseWinner() {
     if (computerSelection === '0') {
 
@@ -60,7 +59,7 @@ function chooseWinner() {
             computerWon += 1;
         } else if (userSelection === 'paper') {
             alert("It's a tie!");
-            console.log("Tie");
+            console.log("Tie")
         } else {
             alert("Congrats, you won this round!");
             console.log("User won");
@@ -83,95 +82,25 @@ function chooseWinner() {
     };
 };
 
-//game if not started when the button is not clicked
-let gameOn = false;
+//add text content div to html
+const container = document.querySelector('#result');
+const content = document.createElement('div');  
+content.setAttribute('id','content');
+container.appendChild(content);
 
-//function to switch on the game
-function game() {
-    alert('Choose Rock, Paper or Scissors. First to 5 win!');
-    //show buttons to let user choose 
-    getUserChoice();
-    if (answer === null) {
-        alert("You've cancelled the game. Refresh the page to start again.");
-    } else if (answer[0].toUpperCase() === 'Y') {
-        gameOn = true;
-        //reset the stats each game
-        let playRound = 0;
-        userWon = 0;
-        computerWon = 0;
-        while (gameOn) {
-            playRound += 1;
-            //print round number
-            msg = console.log("Round " + playRound + ":");
-            getComputerChoice();
-            getUserChoice();
-            chooseWinner();
-            //make the game stops after one user/computer won 5 rounds
-            if (userWon === 5) {
-                alert("Congrats! You won 5 rounds first!");
-                console.log("GAME OVER. User won the game");
-                //display results
-                console.log("User: " + userWon + " Computer: " + computerWon);
-                break;
-            } else if (computerWon === 5) {
-                alert("Uh oh! Computer won 5 rounds first!");
-                console.log("GAME OVER. Computer won the game");
-                //display results
-                console.log("User: " + userWon + " Computer: " + computerWon);
-                break;
-            } else {
-                continue;
-            };
-        };
-    } else if (answer[0].toUpperCase() === 'N') {
-        alert("You've exited the game. Refresh the page to start again.");
-    } else {
-        alert('Invalid answer. Please enter Y or N.');
-    };
+function playRound() {
+    round += 1;
+    //print round number
+    msg = console.log("Round " + round + ":");
+    getComputerChoice();
+    console.log("Computer chooses: " + computerSelection);
+    console.log("User chooses: " + userSelection);
+    chooseWinner();
+    //display result in div
+    content.textContent = 
+    `Round: ${round}
+    Computer chose: ${computerSelection}
+    User chose: ${userSelection}
+    User won: ${userWon}
+    Computer won: ${computerWon}`
 };
-
-//V1 - without button.
-//while (game) {
-
-//    startGame = prompt('Are you ready to play? Y or N: ') ;
-
-    //if user quit by other methods than entering N
-//    if (startGame === null) {
-//        alert("You've cancelled the game. Refresh the page to start again.")
-//        game = false;
-    
-//    } else if (startGame[0].toUpperCase() === 'Y') {
-        //adding round each game
-//        playRound += 1;
-        //print round number
-//        msg = console.log("Round " + playRound + ":");
-//        getComputerChoice();
-//        computerSelection = choices[getComputerChoice()];
-//        getUserChoice();
-//        chooseWinner();
-        //make the game stops after one user/computer won 5 rounds
-//        if (userWon === 5) {
-//            alert("Congrats! You won 5 rounds first!");
-//            console.log("GAME OVER. User won the game");
-            //display results
-//            console.log("User: " + userWon + " Computer: " + computerWon);
-//            break;
-//        } else if (computerWon === 5) {
-//            alert("Uh oh! Computer won 5 rounds first!");
-//            console.log("GAME OVER. Computer won the game");
-            //display results
-//            console.log("User: " + userWon + " Computer: " + computerWon);
-//            break;
-//        } else {
-//            continue;
-//        };
-
-//    } else if (startGame[0].toUpperCase() === 'N') {
-//        alert("You've exited the game. Refresh the page to start again.")
-//        game = false;
-        
-//    } else {
-//        alert('Invalid answer. Please enter Y or N.');
-//        continue;
-//    };
-//};
